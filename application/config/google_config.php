6a2f6f0ab4683963df8cff3299e8bcf55944e2d8
Phumai <mcc3939@gmail.com>
2019-09-24 22:17:56 +0700

    Update google_config.php

--- application/config/google_config.php
+++ application/config/google_config.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['google_client_id']="740234087410-ddl4fuftk7to9d5489uhds1s9pi07ojq.apps.googleusercontent.com";
-$config['google_client_secret']="sjniJJu9ipXZyRinaXSyeZTm";
+$config['google_client_id']="";
+$config['google_client_secret']="";
 $config['google_redirect_url']=base_url().'login/oauth2callback';
 
